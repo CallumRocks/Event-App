c50678eb8ce1462222fc479f35f1f7f26de4f2b5
PANDORA <Pandora@Callum-Rocks.local>
2017-09-14 10:41:08 +0100

    adding state as constructor

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,6 +3,13 @@ import React, { Component } from 'react';
 // Define a new class and give it all the functionality that
 // the React.Component has.
 class SearchBar extends Component {
+    constructor(props) {
+        // "super" class the parent method of the parent class e.g. Component.
+        // if not called in this example an error will be flagged
+        super(props);
+
+        this.state ={ term: '' };
+    }
     // Syntax for finding methods on a class
     // Render function, every class must have one
     // Must return JSX or it will fail
@@ -10,6 +17,9 @@ class SearchBar extends Component {
     // "event" can be named anything
         return <input onChange={(event) => console.log(event.target.value)} />;
     }
-    //test
+    // Each class has a State object
+    // State is plain javascript object that is used to record and react to user events events
+    // Anytime it changes it immedietely re-renders and all of its children
+    // You must initialise state to use it.
 }
 export default SearchBar;
